9ee83147291216f0a9bf970b5a0e8dc7288f7ba4
yipei1028 <104306022@nccu.edu.tw>
2018-03-17 01:14:39 +0800

    console the user's info

--- main.js
+++
@@ -1,7 +0,0 @@
-function getUserInfo() {
-    let account_element = document.getElementById("account");
-    let account = account_element.value;
-    let password_element = document.getElementById("password");
-    let password = account_element.value;
-    alert("帳號：" + account + "\n密碼：" + password);
-}
